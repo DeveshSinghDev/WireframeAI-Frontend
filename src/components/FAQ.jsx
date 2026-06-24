09223986d01a4870126e97be96eb2164d76af742
Devesh Singh <deveshsingh173013@gmail.com>
2026-06-24 17:23:13 +0530

    Make the FAQ section light-themed and add some hover effects

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -36,8 +36,8 @@ function FAQ() {
   };
 
   return (
-    <section className="max-w-4xl mx-auto px-6 py-20">
-      <h2 className="text-4xl font-bold text-center text-white">
+    <section className="max-w-4xl mx-auto px-6 py-20 ">
+      <h2 className="text-4xl font-bold text-center text-black">
         Frequently Asked Questions
       </h2>
 
@@ -49,23 +49,23 @@ function FAQ() {
         {faqs.map((faq, index) => (
           <div
             key={index}
-            className="border border-gray-700 rounded-xl p-5 bg-[#262626]"
+            className="border border-gray-100 rounded-xl p-5 bg-white cursor-pointer transition-all duration-300 hover:shadow-lg hover:border-gray-200 hover:-translate-y-1"
           >
             <button
               onClick={() => toggleFAQ(index)}
-              className="w-full flex justify-between items-center text-left"
+              className="w-full flex justify-between items-center text-left cursor-pointer group"
             >
-              <span className="text-white font-medium">
-                {faq.question}
-              </span>
+            <span className="text-gray-900 font-medium transition-colors duration-300 group-hover:text-orange-500">
+  {faq.question}
+</span>
 
-              <span className="text-orange-400 text-xl">
-                {activeIndex === index ? "−" : "+"}
-              </span>
+              <span className="text-orange-400 text-3xl transition-transform duration-300 group-hover:scale-125">
+  {activeIndex === index ? "−" : "+"}
+</span>
             </button>
 
             {activeIndex === index && (
-              <p className="mt-4 text-gray-300">
+              <p className="mt-4 text-gray-500">
                 {faq.answer}
               </p>
             )}
